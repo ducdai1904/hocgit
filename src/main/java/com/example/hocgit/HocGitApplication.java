68e8d91fa42bf70c96d847de1da097b5386a5592
daipd <daipd@kaopiz.com>
2022-09-14 16:53:08 +0700

    add project

---
+++ src/main/java/com/example/hocgit/HocGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.hocgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HocGitApplication.class, args);
+	}
+
+}
