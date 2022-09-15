fa7233d30afc3a0cea4c2d57f1ed56b45bbc45b4
daipd <daipd@kaopiz.com>
2022-09-15 11:43:59 +0700

    add user controller
     #add method update

--- src/main/java/com/example/hocgit/UserController.java
+++ src/main/java/com/example/hocgit/UserController.java
@@ -2,4 +2,20 @@ package com.example.hocgit;
 
 public class UserController {
     private String name;
+    private String age;
+    private String address;
+
+    public String test2(String a, String b){
+        return null;
+    }
+
+    public void add(){
+        String ten;
+        String tuoi;
+    }
+    public void update(){
+        String ten;
+        String tuoi;
+    }
+
 }
