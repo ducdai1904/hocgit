c722eca432604c96995404b36f69d68fc323448d
daipd <daipd@kaopiz.com>
2022-10-03 14:55:39 +0700

    add code UserController

--- src/main/java/com/example/hocgit/UserController.java
+++ src/main/java/com/example/hocgit/UserController.java
@@ -4,4 +4,8 @@ public class UserController {
     private String name;
     private String age;
     private String address;
+
+    public String test2(String a, String b){
+        return null;
+    }
 }
